074ba675c73fafacf1bec22c4ad184d4e5949849
Mario Arturo Serrano Flores <mario.serrano@imssbienestar.gob.mx>
2025-04-09 07:36:20 -0700

    fix ambientes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://app-service-production-2c8e.up.railway.app/api'
+    apiUrl: 'https://app-service-production-2c8e.up.railway.app'
 };
