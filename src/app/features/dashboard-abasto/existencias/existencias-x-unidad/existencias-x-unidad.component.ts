f70389b89e26b25a6bd90f8fb12c6bc58db71039
Mario Arturo Serrano Flores <mario.serrano@imssbienestar.gob.mx>
2025-08-05 09:41:50 -0700

    agregando grupo terapeutico por archivo de CPM

--- src/app/features/dashboard-abasto/existencias/existencias-x-unidad/existencias-x-unidad.component.ts
+++ src/app/features/dashboard-abasto/existencias/existencias-x-unidad/existencias-x-unidad.component.ts
@@ -12,7 +12,7 @@ import { StorageSolicitudService } from '../../../../services/storage-solicitud.
 import { AlertCircleIcon, HospitalIcon, InfoIcon, LucideAngularModule, SheetIcon, TriangleAlertIcon } from 'lucide-angular';
 import { FormsModule } from '@angular/forms';
 import { StorageVariables } from '../../../../shared/storage-variables';
-import { CPMS } from '../../../../models/CPMS';
+import { ClaveGrupo, CPMS } from '../../../../models/CPMS';
 import { Cita } from '../../../../models/Cita';
 import { clasificacionMedicamentosData } from '../../../../models/clasificacionMedicamentosData';
 import { ClasificadorVEN } from '../../../../models/clasificador-ven';
@@ -69,6 +69,7 @@ export class ExistenciasXUnidadComponent implements OnInit, OnChanges, OnDestroy
     };
     claveModalVisible = false;
     claveSeleccionadaModal: string = '';
+    claveGrupos: ClaveGrupo[] = [];
 
     public doughnutChartOptions: ChartOptions<'doughnut'> = {
         responsive: true,
@@ -136,6 +137,7 @@ export class ExistenciasXUnidadComponent implements OnInit, OnChanges, OnDestroy
             // agregar el resumen estatal al inicio del array
             this.unidades.unshift(resumenEstatal);
         }
+        this.claveGrupos = this.storageService.getClaveGruposFromLocalStorage();
     }
 
     ngOnDestroy(): void {
@@ -330,6 +332,10 @@ export class ExistenciasXUnidadComponent implements OnInit, OnChanges, OnDestroy
         return clasificacion ? ClasificadorVEN[clasificacion.ven] : '-';
     }
 
+    obtenerGrupoTerapeutico(clave: string): string {
+        return this.claveGrupos.find(grupo => grupo.clave === clave)?.grupoTerapeutico ?? '';
+    }
+
     obtenerDescripcion(clave: string): string {
         return this.articulosMap.get(clave)?.descripcion || '-';
     }
@@ -406,9 +412,7 @@ export class ExistenciasXUnidadComponent implements OnInit, OnChanges, OnDestroy
         const resumen = this.resumenCPMs();
         const disponibles = resumen.totalClaveDisponibles;
         const faltantes = this.cpmsElegidos.length - disponibles;
-        const totalPiezasDisponibles = resumen.totalPiezasDisponibles;
-
-        const claveGrupos = this.storageService.getClaveGruposFromLocalStorage();
+        const totalPiezasDisponibles = resumen.totalPiezasDisponibles;        
 
         // Formato anónimo requerido
         const existenciasData = this.cpmsElegidos.map((cpm, index) => {
@@ -421,8 +425,8 @@ export class ExistenciasXUnidadComponent implements OnInit, OnChanges, OnDestroy
                             existenciaAlmacenes.existenciasAZE - existenciaAlmacenes.existenciasAZM - 
                             existenciaAlmacenes.existenciasAZT,
                  0);
-            const gpo = claveGrupos.find(grupo => grupo.clave === cpm.clave)?.gpo ?? '';
-            const grupoTerapeutico = claveGrupos.find(grupo => grupo.clave === cpm.clave)?.grupoTerapeutico ?? '';
+            const gpo = this.claveGrupos.find(grupo => grupo.clave === cpm.clave)?.gpo ?? '';
+            const grupoTerapeutico = this.claveGrupos.find(grupo => grupo.clave === cpm.clave)?.grupoTerapeutico ?? '';
 
             return {
                 clave: cpm.clave,
